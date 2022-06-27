afa4298c6da846c97db634b84b6b324d40e8e06d
Francis Aweenagua <faweenagua@gmail.com>
2022-06-27 18:48:47 +0000

    started info section

---
+++ src/components/Info.js
@@ -0,0 +1,12 @@
+import React from "react";
+import profilePic from "../images/profile.jpg";
+
+export default function Info() {
+  return (
+    <div className="info">
+      <img src={profilePic} alt="profile" className="profile-picture" />
+      <h2 className="user-name">Francis Aweenagua</h2>
+      <h3 className="user-role">Frontend Developer</h3>
+    </div>
+  );
+}
