b98692a8f5b7a2216ab63c0c42a06301f9d29656
Francis Aweenagua <faweenagua@gmail.com>
2022-06-27 18:59:50 +0000

    updated info section

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,6 +7,7 @@ export default function Info() {
       <img src={profilePic} alt="profile" className="profile-picture" />
       <h2 className="user-name">Francis Aweenagua</h2>
       <h3 className="user-role">Frontend Developer</h3>
+      <h5 className="user-email">faweenagua@gmail.com</h5>
     </div>
   );
 }
