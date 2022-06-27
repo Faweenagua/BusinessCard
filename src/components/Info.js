104e2b10d50471d8b0effe83fdea74d4851c8e33
Francis Aweenagua <faweenagua@gmail.com>
2022-06-27 22:23:32 +0000

    completed info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,9 @@
 import React from "react";
 import profilePic from "../images/profile.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+// import { faSolid } from "@fortawesome/free-solid-svg-icons";
 
 export default function Info() {
   return (
@@ -8,6 +12,20 @@ export default function Info() {
       <h2 className="user-name">Francis Aweenagua</h2>
       <h3 className="user-role">Frontend Developer</h3>
       <h5 className="user-email">faweenagua@gmail.com</h5>
+      <div className="buttons">
+        <button className="button-email">
+          <span className="button-email_icon">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
+          <span className="button-email_email">Email</span>
+        </button>
+        <button className="button-linkedin">
+          <span className="button-linkedin_icon">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </span>
+          <span className="button-linkedin_linkedin">LinkedIn</span>
+        </button>
+      </div>
     </div>
   );
 }
