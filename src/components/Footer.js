24d0e27d4fa1563c558354d680f7832227eaa4aa
Francis Aweenagua <faweenagua@gmail.com>
2022-06-28 19:26:07 +0000

    completed footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,36 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faTwitter,
-  faFacebook,
-  faSnapchat,
-  faGithub,
+  faTwitterSquare,
+  faFacebookSquare,
+  faSnapchatSquare,
+  faGithubSquare,
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
-    <div className="footer">
+    <div className="footer-section">
       <a href="twitter.com">
-        <span className="button-email_icon">
-          <FontAwesomeIcon icon={faTwitter} />
+        <span className="button-icon button-email_icon">
+          <FontAwesomeIcon icon={faTwitterSquare} />
         </span>
       </a>
 
       <a href="facebook.com">
-        <span className="button-email_icon">
-          <FontAwesomeIcon icon={faFacebook} />
+        <span className="button-icon button-email_icon">
+          <FontAwesomeIcon icon={faFacebookSquare} />
         </span>
       </a>
 
       <a href="snapchat.com">
-        <span className="button-email_icon">
-          <FontAwesomeIcon icon={faSnapchat} />
+        <span className="button-icon button-email_icon">
+          <FontAwesomeIcon icon={faSnapchatSquare} />
         </span>
       </a>
 
       <a href="github.com">
-        <span className="button-email_icon">
-          <FontAwesomeIcon icon={faGithub} />
+        <span className="button-icon button-email_icon">
+          <FontAwesomeIcon icon={faGithubSquare} />
         </span>
       </a>
     </div>
